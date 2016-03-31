877b8d51f45342fc30c743c0c9e89f7d7d76d880
leoy <420303844@qq.com>
2016-03-31 11:28:42 +0800

    addfile

---
+++ js/mf.js
@@ -0,0 +1,30 @@
+var MF = MF || {}
+
+MF.vars = {
+	h:272,
+	nav:document.getElementById("nav"),
+	content:document.getElementById("content"),
+}
+
+MF.utils = {
+	showMenu:function(){
+		MF.vars.nav.style.display = 'block';
+		MF.vars.nav.style.transformOrigin = '0px 0px 0px';
+		MF.vars.content.style.webkitTransition = '0';
+		MF.vars.content.style.transition = '0';
+		MF.vars.content.style.webkitTransform = "translate3d(" + MF.vars.h + "px, 0, 0)";
+		MF.vars.content.style.transform = "translate(" + MF.vars.h + "px, 0)";
+
+		var lay = document.createElement('div');
+		lay.className = "msite_overlay";
+		MF.vars.content.appendChild(lay)
+
+		lay.on
+	}
+}
+
+;(function(){
+	$(".menu").on('click',function(){
+	   MF.utils.showMenu();
+	})
+})()
