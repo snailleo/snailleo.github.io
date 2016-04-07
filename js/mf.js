689a8b5173910697199e15eb8b41bac58098af70
leoyyy <420303844@qq.com>
2016-04-07 21:44:35 +0800

    修改

--- js/mf.js
+++ js/mf.js
@@ -11,9 +11,10 @@ MF.utils = {
 		MF.vars.nav.style.display = 'block';
 		MF.vars.nav.style.transformOrigin = '0px 0px 0px';
 		MF.vars.content.style.webkitTransition = '0';
-		MF.vars.content.style.transition = '0';
-		MF.vars.content.style.webkitTransform = "translate3d(" + MF.vars.h + "px, 0, 0)";
-		MF.vars.content.style.transform = "translate(" + MF.vars.h + "px, 0)";
+		// MF.vars.content.style.transition = '0';
+		// MF.vars.content.style.webkitTransform = "translate3d(" + MF.vars.h + "px, 0, 0)";
+		// MF.vars.content.style.transform = "translate(" + MF.vars.h + "px, 0)";
+		MF.vars.content.classList.add('in')
 
 		var lay = document.createElement('div');
 		lay.className = "msite_overlay";
@@ -22,7 +23,8 @@ MF.utils = {
 		lay.onclick = function(){
 			$(this).remove();
 			MF.vars.nav.style.display = 'none';
-			MF.vars.content.style.webkitTransform = "translate3d(0, 0, 0)";
+			// MF.vars.content.style.webkitTransform = "translate3d(0, 0, 0)";
+			MF.vars.content.classList.remove('in');
 		}
 	}
 }
