6ff21b6aa010659fad799a625c8ceee052764c4a
leoyyy <420303844@qq.com>
2016-04-08 21:46:19 +0800

    add

--- js/sel.js
+++ js/sel.js
@@ -4,6 +4,7 @@ S.vars = {
 	h:51,
 	close:document.getElementById("close"),
 	content:document.getElementById("selColor"),
+	gList:document.getElementById('gList')
 }
 
 S.utils = {
@@ -14,6 +15,9 @@ S.utils = {
 		S.vars.content.style.transition = '0';
 		S.vars.content.style.webkitTransform = "translate3d(0,0, 0)";
 		S.vars.content.style.transform = "translate(0,0)";
+		
+		S.vars.gList.style.display = 'block';
+		getId("gSucc").style.display = 'none';
 
 		var lay = document.createElement('div');
 		lay.className = "msite_overlay";
@@ -27,12 +31,22 @@ S.utils = {
 		$(this).remove();
 		S.vars.content.style.display = 'none';
 		S.vars.content.style.webkitTransform = "translate3d(0, 0, 0)";
+		//视作从添加购物车成功浮层空白处点击
+		if (getId("gList").style.display == 'none') {
+			$('.guige input[name="skuId"]').val('');
+			$('.list_bot .shop_num span').html(1);
+			$('.detail_dl p').html($('input[name="minPrice"]').attr('val'));
+			var skuOptionArray = $('.kuai span.on');
+			skuOptionArray.each(function(){
+				$(this).removeClass('on');
+			});
+		}
 	},
 	bindDom:function(){
-		getId("add_shop").addEventListener('click',function(){
-			getId("gList").style.display = 'none';
-			getId("gSucc").style.display = 'block';
-		})
+//		getId("add_shop").addEventListener('click',function(){
+//			getId("gList").style.display = 'none';
+//			getId("gSucc").style.display = 'block';
+//		})
 	}
 }
 
