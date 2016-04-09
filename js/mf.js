3893b122d31da59908d3184bdf7c70a5080f2181
leoyyy <420303844@qq.com>
2016-04-10 00:41:47 +0800

    add

--- js/mf.js
+++ js/mf.js
@@ -9,6 +9,7 @@ MF.vars = {
 MF.utils = {
 	showMenu:function(){
 		MF.vars.nav.style.display = 'block';
+		MF.vars.nav.style.zIndex = '0';
 		MF.vars.nav.style.transformOrigin = '0px 0px 0px';
 		// MF.vars.content.style.webkitTransition = '0';
 		// MF.vars.content.style.transition = '0';
@@ -25,14 +26,15 @@ MF.utils = {
 
 		lay.onclick = function(){
 			$(this).remove();
+			MF.vars.nav.style.zIndex = '-1';
 			MF.vars.content.classList.remove('in');
 			setTimeout(function(){
 				MF.vars.nav.style.display = 'none';
 			},800)
 		}
 	},
-	setLi:function(){
-		var li = document.querySelectorAll('.scroll_wrap li');
+	setLi:function(cla){
+		var li = document.querySelectorAll('.'+cla+' li');
 		var str = '';
 		for(var i=0;i<li.length;i++){
 			if(i == 0){
@@ -45,8 +47,44 @@ MF.utils = {
 	}
 }
 
+function tanchu(text,type){
+	var _z=9999999,_html;	
+	// var _obj= $("<div class='tanchu'>"+text+"</div>");
+	var _obj = document.createElement('div');
+	_obj.className = 'tanchu';
+	_obj.id = 'tanchu';
+
+	if(type==1){
+		_html = '<span class="tsucc"></span><p>'+text+'</p>'
+	}else if(type == 2){
+		_html = '<span class="ttan"></span><p>'+text+'</p>'
+	}else{
+		_html = '<p>'+text+'</p>'
+	}
+
+	document.body.appendChild(_obj);
+	_obj.innerHTML = _html;
+
+	var left=($(window).width()-$(".tanchu").width())/2+'px';
+	var top =($(window).height()-$(".tanchu").width())/2+'px';
+	$(".tanchu").css({	"left":left,"top":top,"display":"block","z-index":_z-(-1)});
+}
+
+function pop_box(text,type){
+	tanchu(text,type);
+	setTimeout(function(){
+		$(".tanchu").remove();	
+	},3000)
+}
+
 ;(function(){
 	$(".menu").on('click',function(){
 	   MF.utils.showMenu();
 	})
 })()
+
+// var myscroll;
+// function loaded(){
+// 	myscroll=new iScroll("nav");
+// }
+// window.addEventListener("DOMContentLoaded",loaded,false)
